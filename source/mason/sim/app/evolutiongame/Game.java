f412d050bbacc04e4881fb27fd8358891b0bc939
ben <ben@ben-Z68MA-D2H-B3>
2015-01-20 01:22:02 -0800

    -Added a package for my work and a Game class to start building. -Put in the MASON pdf manual so I don't need to search for it online again -formatted the Heap class cause the default formatting in here is really stupid

---
+++ source/mason/sim/app/evolutiongame/Game.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sim.app.evolutiongame;
+
+/**
+ * This class will hold a payoff matrix/payoff matrices for a two player game.
+ * As well, it will have some helper methods to find useful facts such as the
+ * set of Nash Equilibrium in a game.
+ * @author ben
+ */
+public class Game
+{
+    
+}
